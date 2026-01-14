9f3e4d9c3ab1ff798f8bff000fccbad634295e73
Erik Forsgren <crapmannen@hotmail.com>
2026-01-14 08:52:26 +0100

    Small visual fixes.

--- MauiProjectBWeather/MainPage.xaml.cs
+++ MauiProjectBWeather/MainPage.xaml.cs
@@ -28,17 +28,5 @@ namespace MauiProjectBWeather
             }
         }
 
-
-        //private void OnCounterClicked(object sender, EventArgs e)
-        //{
-        //    count++;
-
-        //    if (count == 1)
-        //        CounterBtn.Text = $"Clicked {count} time";
-        //    else
-        //        CounterBtn.Text = $"Clicked {count} times";
-
-        //    SemanticScreenReader.Announce(CounterBtn.Text);
-        //}
     }
 }
